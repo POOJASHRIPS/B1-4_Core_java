332417644af8b2cec652d265015ec83050a4c528
Poojashri <Poojashri@DESKTOP-4BQNJRO>
2022-04-05 22:41:20 +0530

    First commit

---
+++ Day2_ intro_to_java/src/com/tns/introduction/Example1.java
@@ -0,0 +1,16 @@
+package com.tns.introduction;
+
+import java.util.Scanner;
+
+public class Example1 {
+
+	static public void main(String[] args) 
+	{
+		Scanner s=new Scanner(System.in);
+		char c=s.next().charAt(4);
+		System.out.println(c);
+		s.close();
+
+	}
+
+}
