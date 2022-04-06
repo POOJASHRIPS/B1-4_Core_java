0f33b2b98ec8a9a8fe0beb80aeb13663d0d02b3d
Poojashri <Poojashri@DESKTOP-4BQNJRO>
2022-04-06 11:12:57 +0530

    commit

---
+++ Assignment1/src/looping/WhileLoop.java
@@ -0,0 +1,19 @@
+
+		package looping;
+
+		public class WhileLoop {
+			public static void main(String[] args)
+			{
+				int n=1;
+				while(n<=9)
+				{
+					System.out.println(n);
+					n++;
+				}
+				
+
+			}
+		}
+	}
+
+}
