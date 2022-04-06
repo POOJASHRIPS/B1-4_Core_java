fcb7a88f6ef73b3185697c5609d83a2ef75dad0b
Poojashri <Poojashri@DESKTOP-4BQNJRO>
2022-04-06 21:03:54 +0530

    First commit

---
+++ Day_8/src/Functionoverloading.java
@@ -0,0 +1,26 @@
+
+public class Functionoverloading {
+	package polymorphism;
+	//by changing number of arguments
+	public class FunctionOverloading {
+
+		
+		void assign(int a)
+		{
+			
+			System.out.println("Ara of a square: "+(a*a));
+		}
+		
+		void assign(int a, int b)
+		{
+			
+			System.out.println("Ara of a rectangle: "+(a*b));
+		}
+		public static void main(String[] args) {
+			FunctionOverloading o = new FunctionOverloading();
+			o.assign(5);
+			o.assign(11,12);
+		}
+
+	}
+}
