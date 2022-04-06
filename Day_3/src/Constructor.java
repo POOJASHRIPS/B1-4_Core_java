8d261cac4acf120275df007b7d5ad8b89e6cbc24
Poojashri <Poojashri@DESKTOP-4BQNJRO>
2022-04-06 09:29:35 +0530

    commit

--- Day_3/src/Constructor.java
+++ Day_3/src/Constructor.java
@@ -1,4 +1,29 @@
 
 public class Constructor {
+	package day3;
 
+	public class Constructor {
+
+		String str;
+		int a;
+		Constructor(String st,int x)
+		{
+			str=st;
+			a=x;
+		}
+		void display()
+		{
+			System.out.println(str+" "+a);
+			
+		}
+		public static void main(String[] args) {
+			// TODO Auto-generated method stub
+			Constructor c=new Constructor("Sahana",50);
+			Constructor c1=new Constructor("Pooja",25);
+			c.display();
+			c1.display();
+		}
+
+	}
+	
 }
