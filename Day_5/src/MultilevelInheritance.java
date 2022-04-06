0f360f3fd6c6381ef02d876afdb7e80dd467d270
Poojashri <Poojashri@DESKTOP-4BQNJRO>
2022-04-06 11:06:38 +0530

    commit

--- Day_5/src/MultilevelInheritance.java
+++ Day_5/src/MultilevelInheritance.java
@@ -2,8 +2,36 @@
 public class MultilevelInheritance {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		package Day_5;
 
+		class R6{
+			void print() {
+				System.out.println("I Like R6");
+			}
+		}
+
+		class R3 extends R6{
+			
+			void display() {
+				System.out.println("I Like R3");
+			}
+		}
+
+		class R15 extends R3{
+			void assign() {
+				System.out.println("I Like R15");
+			}
+		}
+		public class MutlilevelInheritance {
+
+			public static void main(String[] args) {
+				R15 r =new R15();
+				r.display();
+				r.assign();
+				r.print();
+			}
+
+		}
 	}
 
 }
