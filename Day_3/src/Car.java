32b0ded416659635ea482f5e169b855d9d0f886f
Poojashri <Poojashri@DESKTOP-4BQNJRO>
2022-04-06 09:25:16 +0530

    First commit

--- Day_3/src/Car.java
+++ Day_3/src/Car.java
@@ -1,4 +1,17 @@
 
 public class Car {
+	package day_3;
 
+	public class Car {
+
+		void display()
+		{
+			System.out.println("Welcome to c2tc");
+		}
+		public static void main(String[] args) {
+			Car c= new Car();
+			c.display();
+	}
+
+	}
 }
