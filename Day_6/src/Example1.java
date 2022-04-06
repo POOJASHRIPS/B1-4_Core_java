7671b83977f88c1a45223fa018b532496b09738a
Poojashri <Poojashri@DESKTOP-4BQNJRO>
2022-04-06 11:10:13 +0530

    First commit

---
+++ Day_6/src/Example1.java
@@ -0,0 +1,33 @@
+
+package Day_6;
+
+abstract class Animal
+{
+	abstract void eat();
+}
+class Herbivores extends Animal
+{
+	void eat()
+	{
+		System.out.println("I am vegetarian");
+	}
+}
+class Carnivores extends Animal
+{
+	void eat()
+	{
+		System.out.println("I am non-vegetarian");
+	}
+}
+
+public class Example1 {
+
+	public static void main(String[] args) {
+		Animal h=new Herbivores();
+		Animal c=new Carnivores();
+		h.eat();
+		c.eat();
+
+	}
+
+}
