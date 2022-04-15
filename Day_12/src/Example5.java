965c225e681783e5e10ab3c9c35b5de7da856c22
Poojashri <Poojashri@DESKTOP-4BQNJRO>
2022-04-15 17:26:28 +0530

    First commit

---
+++ Day_12/src/Example5.java
@@ -0,0 +1,20 @@
+
+class Thread1 extends Thread
+{
+	public void run()
+	{
+		System.out.println("Thread interruption");
+	}
+}
+public class Example5
+{
+	public static void main(String[] args)
+	{
+		Thread1 t1=new Thread1();
+		t1.start();
+		System.out.println("is thread interrupted "+t1.isInterrupted());
+		t1.interrupt();
+		System.out.println("is thread interrupted "+t1.isInterrupted());
+	}
+
+}
